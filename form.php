d51af595559fb2c91ad233f69b988e5e9238a226
jeronimo_nuñez <jeronimonunez96@gmail.com>
2024-09-16 14:26:50 -0300

    correccion del form

--- form.php
+++ form.php
@@ -2,22 +2,29 @@
 
 $nombre = $_POST['name'];
 $mail = $_POST['email'];
-$telefono = $_POST['tel'];
+$telefono = $_POST['phone'];  // Cambiado de 'tel' a 'phone'
 $mensaje = $_POST['Mensaje'];
 
-//Cuerpo del mensaje que envien los usuarios
+// Cuerpo del mensaje que envían los usuarios
 $mensaje = "Mensaje enviado por " . $nombre . ",\r\n";
 $mensaje .= "su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Telefono de contacto: " .$tel . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['Mensaje'] . " \r\n";
+$mensaje .= "Telefono de contacto: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $mensaje . " \r\n";
 $mensaje .= "Enviado el día " . date('d/m/Y', time());
 
 $para = 'jeronimonunez2405@gmail.com';
-$asunto = 'Enviado desde el Sitio Web Tierras Nuestras'
+$asunto = 'Enviado desde el Sitio Web Tierras Nuestras';
 
-//función mail = a quién le envio el mail
-mail($para, $asunto, utf8_decode($mensaje), $header);
+// Encabezados
+$header =   'From: ' . $mail . "\r\n" .
+            'Reply-To: ' . $mail . "\r\n" .
+            'X-Mailer: PHP/' . phpversion() . "\r\n" .
+            'Content-Type: text/plain; charset=UTF-8' . "\r\n";
 
-//Redirección al header despues de enviado el form
-header('Location:exito.html');
+// Función mail
+if (mail($para, $asunto, utf8_decode($mensaje), $header)) {
+    header('Location: exito.html');
+} else {
+    echo 'Error al enviar el correo.';
+}
 ?>
